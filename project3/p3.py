eb8788eb89944efcb9922b091f15f1fbc94def8c
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2021-02-01 11:52:18 -0500

    Update pseudocode

--- project3/p3.py
+++ project3/p3.py
@@ -31,40 +31,41 @@ bot = qbot(speed)
 Pseudocode!!!
 
 Function Dispense Container
-    In  <- Container ID
-    Determine Attributes from ID
+    Input  <- Container ID
+    Determine Container Attributes from ID
     Dispense container in Sorting Station
-    Out -> Container Attributes
-End Function
-
-Function Check Pickup Condition
-    In  -> Current QBot State, Container Attributes
-    Out -> (Number, Destination, Total Mass) all ok
+    Output -> Container Attributes
 End Function
 
 Function Load Container
-    In  -> Container Attributes
-    If Check Pickup Condition is not met
-        Exit the Function
+    Input  -> Container Attributes
+    If Qbot already has 3 containers
+        OR Destination is different
+        OR Total mass exceeds 90 grams
+            Exit the Function
     Else
-        Pick up container from Sorting Station
-        Drop off container onto QBot
+        QArm picks up container from Sorting Station
+        QArm drops off container onto QBot
 End Function
 
 Function Transfer Container
-    In -> Bin Number, Sensor Type
+    Input -> Bin Number, Sensor Type
     While Sensor for Bin Number not Activated
         Move QBot forward
 End Function
 
 Function Deposit Container
-    Turn and Move Qbot until at Bin
+    Turn QBot towards bin
+    Move Qbot forward until a distance threshold
+    Turn QBot to Align Hopper to Bin
     [Optional: Use File Data]
     Rotate Hopper to Deposit
+    Move QBot Back to Yellow Line
 End Function
 
 Function Return Home
-    QBot Follows Line until End of Line
+    While not at the end of the line
+        QBot follows line in a loop
     Rotate QBot around
 End Function
 
@@ -141,6 +142,7 @@ def return_home():
     bot.stop()
     bot.rotate(180)
 
+bot.read_green_color_sensor()
 ##---------------------------------------------------------------------------------------
 ## STUDENT CODE ENDS
 ##---------------------------------------------------------------------------------------
